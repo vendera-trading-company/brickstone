a499c7b9a6109e1817647ccf188db1a4e34bf633
Faruk Ertürk Business <95526628+ertuerk-faruk-business@users.noreply.github.com>
2024-02-02 08:42:46 +0000

    deploy v1.0.2

--- example/src/main.js
+++ example/src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
-import * as brickstone from "brickstone";
 import "brickstone/dist/style.css";
+
+import * as brickstone from "brickstone";
 import installer from 'brickstone/dist/installer';
 
 createApp(App).use(installer, brickstone).mount("#app");
